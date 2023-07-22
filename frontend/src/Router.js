3320c7014185858f994cd5efd1148224e241e01a
MasterMind7777777 <go.go.nest.nest@gmail.com>
2023-07-23 00:10:10 +0300

    recomend transdaction component layout

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -9,17 +9,19 @@ import Navbar from './components/Common/Navbar';
 import UserList from './components/User/UserList';
 import Login from './components/User/Login';
 import Logout from './components/User/Logout';
-// Transaction
+// Transaction CRUD
 import TransactionDetail from './components/Transaction/TransactionDetail';
 import AddTransaction from './components/Transaction/AddTransaction';
 import UpdateTransaction from './components/Transaction/UpdateTransaction';
 import TransactionListWithAdd from './components/Transaction/TransactionListWithAdd';
-// Budget
+// Transaction Custom
+import RecommendationComponent from './components/Transaction/RecommendationComponent';
+// Budget CRUD
 import BudgetListWithAdd from './components/Budget/BudgetListWithAdd';
 import BudgetDetail from './components/Budget/BudgetDetail';
 import CreateBudget from './components/Budget/CreateBudget';
 import UpdateBudget from './components/Budget/UpdateBudget';
-// Category
+// Category CRUD
 import CategoryListWithAdd from './components/Category/CategoryListWithAdd';
 import CategoryDetail from './components/Category/CategoryDetail';
 import AddCategory from './components/Category/AddCategory';
@@ -30,21 +32,27 @@ function AppRouter() {
     <Router>
       <Navbar />
       <Routes>
+        {/* User */}
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<ProtectedRoute><Logout /></ProtectedRoute>} />
         <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/users" element={<ProtectedRoute><UserList /></ProtectedRoute>} />
+        {/* Transaction CRUD */}
         <Route path="/transactions" element={<ProtectedRoute><TransactionListWithAdd /></ProtectedRoute>} />
         <Route path="/transactions/:id" element={<ProtectedRoute><TransactionDetail /></ProtectedRoute>} />
         <Route path="/transactions/add" element={<ProtectedRoute><AddTransaction /></ProtectedRoute>} />
         <Route path="/transactions/update/:id" element={<ProtectedRoute><UpdateTransaction /></ProtectedRoute>} />
+        {/* Transaction Custom */}
+        <Route path="transactions/recommendations" element={<ProtectedRoute><RecommendationComponent numRecommendations={5} /></ProtectedRoute>} />
+        {/* Budget CRUD */}
         <Route path="/budgets" element={<ProtectedRoute><BudgetListWithAdd /></ProtectedRoute>} />
         <Route path="/budgets/:id" element={<ProtectedRoute><BudgetDetail /></ProtectedRoute>} />
-        <Route path="/budgets/add" element={<ProtectedRoute><CreateBudget /></ProtectedRoute>} />
+        <Route path="/budgets/create" element={<ProtectedRoute><CreateBudget /></ProtectedRoute>} />
         <Route path="/budgets/update/:id" element={<ProtectedRoute><UpdateBudget /></ProtectedRoute>} />
+        {/* Category CRUD */}
         <Route path="/categories" element={<ProtectedRoute><CategoryListWithAdd /></ProtectedRoute>} />
         <Route path="/categories/:id" element={<ProtectedRoute><CategoryDetail /></ProtectedRoute>} />
-        <Route path="/categories/create" element={<ProtectedRoute><AddCategory /></ProtectedRoute>} />
+        <Route path="/categories/add" element={<ProtectedRoute><AddCategory /></ProtectedRoute>} />
         <Route path="/categories/:id/edit" element={<ProtectedRoute><UpdateCategory /></ProtectedRoute>} />
         {/* Add more protected routes as needed */}
       </Routes>
---
+++ frontend/src/components/Transaction/RecommendationComponent.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { getTransactionRecommendations } from '../../api/transaction';
+
+const RecommendationComponent = ({ numRecommendations }) => {
+  const [recommendations, setRecommendations] = useState([]);
+  const [mostUsedCategory, setMostUsedCategory] = useState(null);
+
+  useEffect(() => {
+    getTransactionRecommendations(numRecommendations)
+      .then(response => {
+        console.log(response);
+        if (response) {
+          setRecommendations(response.recommendations);
+          setMostUsedCategory(response.most_used_category);
+        }
+      })
+      .catch(error => {
+        console.error('Failed to fetch transaction recommendations:', error);
+      });
+  }, [numRecommendations]);
+
+  return (
+    <div>
+      <h2>Your transaction recommendations based on the most used category: {mostUsedCategory}</h2>
+      <ul>
+        {recommendations.map((recommendation, index) => (
+          <li key={index}>
+            <h3>{recommendation.title}</h3>
+            <p>{recommendation.description}</p>
+            <p>Amount: {recommendation.amount}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecommendationComponent;
